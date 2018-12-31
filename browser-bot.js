4acfc32168a8714f6a187f82a65bdbbfb558e867
Joe <jox.rox.js@gmail.com>
2018-12-31 02:07:07 -0500

    Fix for static fields in browser-bot and added support for user profiles for autofill

--- browser-bot.js
+++ browser-bot.js
@@ -11,18 +11,8 @@ const fkill = require('fkill');
 const autofill = require("./autofill.json")
 const config = require("./config.json");
 
-let proxy;
-let instance;
-let baseUrl;
-let browser;
-let page;
-let headedBrowser = null;
-let page2 = null;
-
-//Used to prevent multiple autofills
-var lastAutoFill1 = 0;
-var lastAutoFill2 = 0;
-var delay = 10000;
+var checkoutUrl = 'https://www.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/COShipping-Show';
+var paymentUrl = 'https://www.adidas.com/on/demandware.store/Sites-adidas-US-Site/en_US/COSummary2-Start';
 
 // For waiting a set number of ms
 let wait = ms => new Promise(resolve => setTimeout(resolve, ms));
@@ -30,18 +20,24 @@ let wait = ms => new Promise(resolve => setTimeout(resolve, ms));
 module.exports = class Bot {
 
     constructor(i, p) {
-        instance = i;
-        proxy = p;
+        this.baseUrl = '';
+        this.browser = null;
+        this.page = null;
+        this.instance = i;
+        this.proxy = p;
     }
 
     async start() {
+
+        console.log(`Starting bot instance ${this.instance}`)
+
         let args;
 
-        if (proxy != null) {
+        if (this.proxy != null) {
             args = [
                 '--no-sandbox',
                 `--window-size=${config.windowWidth},${config.windowHeight}`,
-                `--proxy-server=${proxy}`
+                `--proxy-server=${this.proxy}`
             ];
         } else {
             args = [
@@ -51,10 +47,11 @@ module.exports = class Bot {
         }
 
         // Launch the browser
-        browser = await puppeteer.launch({
+        this.browser = await puppeteer.launch({
             args,
             headless: config.headless,
             ignoreHTTPSErrors: true,
+            userDataDir: path.resolve('saves', 'chrome_' + this.instance)
         });
 
         // Add google cookies to browser if provided
@@ -75,16 +72,16 @@ module.exports = class Bot {
         }
 
         // Create main page
-        page = (await browser.pages())[0];
+        this.page = (await this.browser.pages())[0];
 
         // Max the viewport
-        await page.setViewport({
+        await this.page.setViewport({
             width: 0,
             height: 0
         });
 
         //Set timeout
-        page.setDefaultNavigationTimeout(60000);
+        this.page.setDefaultNavigationTimeout(60000);
 
         // Prepare for the tests (not yet implemented).
         await this.preparePage();
@@ -93,95 +90,155 @@ module.exports = class Bot {
         await this.setListeners();
 
         // Navigate to the page
-        await this.goto(config.url);
+        while (true) {
+            try {
+                await this.page.goto(config.url, { waitUntil: 'networkidle0' });
+                this.baseUrl = await this.page.url();
+                break;
+            } catch (err) {
+                logger.error(this.instance, err);
+                await wait(config.retryDelay)
+            }
+        }
+
+        // Wait for ATC page
+        await Promise.race([
+            this.page.waitForXPath("//*[text() = 'Select size']"),
+            this.page.waitForXPath("//*[text() = 'Add To Bag']"),
+        ]);
+
+        // Log success
+        logger.success(this.instance);
+
+        // Notify user
+        if (config.alertOnCartPage) {
+
+            notifier.notify({
+                title: 'Adidas Bruteforcer',
+                message: `Cart page on instance ${this.instance}}!`,
+                sound: 'Hero',
+                timeout: 60000
+            }, async (err, res, data) => {
+                if (res == 'activate') {
+                    await this.page.bringToFront();
+                }
+            });
+        }
+
+        // if (config.autoATC) {
+        //     while (true) {
+        //         if (await this.cartByRequest())
+        //             break;
+        //         else
+        //             await wait(config.retryDelay)
+        //     }
+        //     await this.page.goto(checkoutUrl, { waitUntil: 'domcontentloaded' });
+        // }
+
+
+        // if (config.autofillCheckout) {
+        //     await this.fillCheckout1();
+        //     await (await this.page.$('[name="dwfrm_shipping_submitshiptoaddress"]')).click();
+        //     await this.page.waitForNavigation( { waitUntil: 'domcontentloaded' });
+        //     await this.fillCheckout2();
+        // }
+
 
     }
 
     async stop() {
-        await browser.close();
-        if (headedBrowser != null) await headedBrowser.close();
-    }
-
-    async goto(url) {
-        while (true) {
-            try {
-                await page.goto(url);
-                baseUrl = await page.url();
-                return;
-            } catch (err) {
-                logger.error(instance, err);
-                await wait(config.retryDelay)
-                await this.goto(config.url);
-            }
-
-        }
-
+        await this.browser.close();
     }
 
     // Contains event handlers for various pages and conditions
     async setListeners() {
 
         // Handlers
-        page.on('response', async response => {
+        await this.page.on('response', async response => {
 
-            // Catch cart responses
-            if (response.url().includes("api/cart_items")) {
+            // if (response.url().includes("api/cart_items")) {
+            //     console.log("caught cart response");
 
-            }
-            // Catch page reloads 
-            else if (response.url() == baseUrl) {
+            //     try {
+            //         let json = await response.json();
+            //         if (json.cart.product_quantity != 0) {
+            //             carted = true;
+            //             console.log(`Basket count: ${json.cart.product_quantity}`)
+            //         }
+            //     } catch (ex) {
+            //         console.log(`Error parsing JSON: ${ex}`)
+            //     }
+            // }
 
-                await page.waitForNavigation()
+            // Catch 
+            // else if (response.url() == baseUrl) {
 
-                const sizeSelector = await page.$x("//*[text() = 'Select size']");
-                const cartButton = await page.$x("//*[text() = 'Add To Bag']");
+            //     await page.waitForNavigation()
 
-                // If on cart page
-                if (sizeSelector.length > 0 || cartButton.length > 0) {
+            //     const sizeSelector = await page.$x("//*[text() = 'Select size']");
+            //     const cartButton = await page.$x("//*[text() = 'Add To Bag']");
 
-                    logger.success(instance);
+            //     // If on cart page
+            //     if (sizeSelector.length > 0 || cartButton.length > 0) {
 
-                    // Transfer cookies to headed browser
-                    if (config.headless) {
+            //         console.log(`Carted: ${carted}`)
+            //         console.log(`Past splash: ${pastSplash}`)
 
-                        const sessionCookies = await page.cookies();
+            //         if (!pastSplash)
+            //             logger.success(instance);
 
-                        await this.lauchHeadedBrowser(sessionCookies);
+            //         // Transfer cookies to headed browser
+            //         if (config.headless && !pastSplash) {
+            //             console.log("transfering cookies")
+            //             const sessionCookies = await page.cookies();
 
-                    }
+            //             await this.lauchHeadedBrowser(sessionCookies);
+
+            //         }
 
 
-                    if (config.alertOnCartPage) {
+            //         // Notify user
+            //         if (config.alertOnCartPage && !pastSplash) {
 
-                        notifier.notify({
-                            title: 'Adidas Bruteforcer',
-                            message: `Cart page on instance ${instance}}!`,
-                            sound: 'Hero',
-                            timeout: 60000
-                        }, async (err, res, data) => {
-                            if (res == 'activate') {
-                                await page.bringToFront();
-                            }
-                        });
-                    }
-                }
+            //             notifier.notify({
+            //                 title: 'Adidas Bruteforcer',
+            //                 message: `Cart page on instance ${instance}}!`,
+            //                 sound: 'Hero',
+            //                 timeout: 60000
+            //             }, async (err, res, data) => {
+            //                 if (res == 'activate') {
+            //                     await page.bringToFront();
+            //                 }
+            //             });
+            //         }
 
-            }
+            //         if (config.autoATC && !carted) {
+            //             this.cartByRequest();
+            //         }
+
+            //         // prevent transfer of cookies again
+            //         pastSplash = true;
+
+            //     }
+
+
+
+            // }
 
             // Cart cart page
-            else if (response.url().includes("Cart-Show")) {
-            }
+            // else if (response.url().includes("Cart-Show")) {
+            // }
 
-            // Catch checkout page 1
-            else if (response.url().includes("COShipping-Show")) {
-                if (config.autofillCheckout)
-                    await this.autofillCheckout1();
-            }
-            // Catch checkout page 2
-            else if (response.url().includes("COSummary2-Start")) {
-                if (config.autofillCheckout)
-                    await this.autofillCheckout2();
-            }
+            // // Catch checkout page 1
+            // else if (response.url().includes("COShipping-Show")) {
+            //     if (config.autofillCheckout)
+            //         await this.autofillCheckout1();
+            // }
+            // // Catch checkout page 2
+            // else if (response.url().includes("COSummary2-Start")) {
+            //     if (config.autofillCheckout)
+            //         await this.autofillCheckout2();
+            // }
         });
 
     }
@@ -192,17 +249,17 @@ module.exports = class Bot {
         if (config.randomUserAgent)
             userAgent = new UserAgent({ deviceCategory: 'desktop' }).toString();
 
-        await page.setUserAgent(userAgent);
+        await this.page.setUserAgent(userAgent);
 
         // Pass the Webdriver Test.
-        await page.evaluateOnNewDocument(() => {
+        await this.page.evaluateOnNewDocument(() => {
             Object.defineProperty(navigator, 'webdriver', {
                 get: () => false,
             });
         });
 
         // Pass the Chrome Test.
-        await page.evaluateOnNewDocument(() => {
+        await this.page.evaluateOnNewDocument(() => {
             // We can mock this in as much depth as we need for the test.
             window.navigator.chrome = {
                 runtime: {},
@@ -211,7 +268,7 @@ module.exports = class Bot {
         });
 
         // Pass the Permissions Test.
-        await page.evaluateOnNewDocument(() => {
+        await this.page.evaluateOnNewDocument(() => {
             const originalQuery = window.navigator.permissions.query;
             return window.navigator.permissions.query = (parameters) => (
                 parameters.name === 'notifications' ?
@@ -221,7 +278,7 @@ module.exports = class Bot {
         });
 
         // Pass the Plugins Length Test.
-        await page.evaluateOnNewDocument(() => {
+        await this.page.evaluateOnNewDocument(() => {
             // Overwrite the `plugins` property to use a custom getter.
             Object.defineProperty(navigator, 'plugins', {
                 // This just needs to have `length > 0` for the current test,
@@ -231,7 +288,7 @@ module.exports = class Bot {
         });
 
         // Pass the Languages Test.
-        await page.evaluateOnNewDocument(() => {
+        await this.page.evaluateOnNewDocument(() => {
             // Overwrite the `plugins` property to use a custom getter.
             Object.defineProperty(navigator, 'languages', {
                 get: () => ['en-US', 'en'],
@@ -241,7 +298,11 @@ module.exports = class Bot {
 
     async lauchHeadedBrowser(cookies) {
 
-        headedBrowser = await puppeteer.launch({
+        console.log("launching headed browser")
+
+        this.browser.close();
+
+        this.browser = await puppeteer.launch({
             args: [
                 '--no-sandbox',
                 '--disable-setuid-sandbox',
@@ -255,7 +316,7 @@ module.exports = class Bot {
 
         if (Object.keys(GOOGLE_COOKIES).length != 0) {
 
-            const cookiePage2 = await headedBrowser.newPage();
+            const cookiePage2 = await browser.newPage();
             cookiePage2.setDefaultNavigationTimeout(60000);
 
             await cookiePage2.goto('http://www.google.com/404');
@@ -269,26 +330,70 @@ module.exports = class Bot {
 
         }
 
-        page = (await headedBrowser.pages())[0];
+        this.page = (await browser.pages())[0];
 
-        page.setViewport({ width: 0, height: 0 });
+        this.page.setViewport({ width: 0, height: 0 });
 
-        await page.setCookie(...cookies);
+        // Set cookies
+        await this.page.setCookie(...cookies);
 
-        await this.preparePage(page);
+        // Pass detection
+        await this.preparePage();
 
-        await page.goto(baseUrl);
+        // Set up listeners
+        await this.setListeners();
 
-        await browser.close();
+        await this.page.goto(baseUrl);
+
     }
 
-    async autofillCheckout1() {
+    async cartByRequest() {
 
-        //TODO: Better solution for preventing duplicates
+        // Select the size dropdown to prevent bans when using auto ATC
+        (await this.page.$x("//*[text() = 'Select size']"))[0].click();
+        // May need a delay - drvien by event handler?
+        await wait(2000);
 
-        if (lastAutoFill1 >= (Date.now() - delay))
-            return;
-        lastAutoFill1 = Date.now();
+        let response = await this.page.evaluate(async () => {
+            const res = await fetch("https://www.adidas.com/api/cart_items?sitePath=us", {
+                "credentials": "include",
+                "headers": {
+                    "accept": "*/*",
+                    "accept-language": "en-US,en;q=0.9,fr;q=0.8",
+                    "content-type": "application/json",
+                },
+                "referrer": 'https://www.adidas.com/us/pharrell-williams-bbc-hu-v2-shoes/BB9549.html',
+                "referrerPolicy": "no-referrer-when-downgrade",
+                "body": JSON.stringify({
+                    product_id: "BB9549",
+                    quantity: 1,
+                    product_variation_sku: "BB9549_630",
+                    productId: "BB9549_630",
+                    size: 9,
+                    displaySize: 9,
+                    captchaResponse: ""
+                }),
+                "method": "POST",
+                "mode": "cors"
+            });
+
+            try {
+                const json = await res.json();
+                return json;
+            } catch (e) {
+                return null;
+            }
+        });
+
+        if (response == null) {
+            return false;
+        } else {
+            return response.cart.product_quantity != 0;
+        }
+
+    }
+
+    async fillCheckout1() {
 
         let selectors = [
             "#dwfrm_shipping_shiptoaddress_shippingAddress_firstName",
@@ -312,29 +417,32 @@ module.exports = class Bot {
             autofill.emailAddress
         ]
 
-        await page.waitForNavigation()
-
         for (let i = 0; i < selectors.length; i++) {
-            await page.type(selectors[i], values[i])
-
-            // (await page.$(selectors[i])).click()
-            // await page.keyboard.type(values[i]);
+            try {
+                await this.page.waitForSelector(selectors[i]);
+                await this.page.type(selectors[i], values[i])
+            } catch (e) {
+                logger.error(this.instance, `Failed to autofill field by selector ${selectors[i]}`)
+                return false;
+            }
         }
 
         // Click and select state
-        const state = await page.$x("//*[text() = 'Select State']");
-        await state[0].click();
-        const stateName = await page.$$(`[data-value="${autofill.state}"]`);
-        stateName[0].click();
+        try {
+            const state = await this.page.$x("//*[text() = 'Select State']");
+            await state[0].click();
+            const stateName = await this.page.$$(`[data-value="${autofill.state}"]`);
+            stateName[0].click();
+        } catch (e) {
+            logger.error(this.instance, `Failed to autofill state field`)
+            return false;
+        }
+
+        return true;
 
     }
 
-    async autofillCheckout2() {
-
-        //TODO: Better solution for preventing duplicates
-        if (lastAutoFill2 >= (Date.now() - delay))
-            return;
-        lastAutoFill2 = Date.now();
+    async fillCheckout2() {
 
         let selectors = [
             "#dwfrm_payment_creditCard_number",
@@ -346,24 +454,39 @@ module.exports = class Bot {
             autofill.CVV
         ]
 
-        await page.waitForNavigation()
-
         for (let i = 0; i < selectors.length; i++) {
-            await page.type(selectors[i], values[i])
+            try {
+                await this.page.waitForSelector(selectors[i]);
+                await this.page.type(selectors[i], values[i]);
+            } catch (e) {
+                logger.error(this.instance, `Failed to autofill field by selector ${selectors[i]}`);
+                return false;
+            }
         }
 
         // Click and select month
-        const month = await page.$("#dwfrm_payment_creditCard_month_display_field")
-        await month.click();
-        const monthNumber = await page.$$(`[data-value="${autofill.month}"]`);
-        monthNumber[0].click();
+        try {
+            const month = await this.page.$("#dwfrm_payment_creditCard_month_display_field");
+            await month.click();
+            const monthNumber = await this.page.$$(`[data-value="${autofill.month}"]`);
+            monthNumber[0].click();
+        } catch (e) {
+            logger.error(this.instance, `Failed to autofill month field`);
+            return false;
+        }
 
         // Click and select year
-        const year = await page.$("#dwfrm_payment_creditCard_year_display_field")
-        await year.click();
-        const yearNumber = await page.$$(`[data-value="${autofill.year}"]`);
-        yearNumber[0].click();
+        try {
+            const year = await this.page.$("#dwfrm_payment_creditCard_year_display_field");
+            await year.click();
+            const yearNumber = await this.page.$$(`[data-value="${autofill.year}"]`);
+            yearNumber[0].click();
+        } catch (e) {
+            logger.error(this.instance, `Failed to autofill year field`);
+            return false;
+        }
 
+        return true;
     }
 
 }
--- index.js
+++ index.js
@@ -1,16 +1,46 @@
 const config = require("./config.json");
 const Bot = require('./browser-bot.js');
 const logger = require('./logger.js');
-var fs = require('fs');
+const fs = require('fs-extra')
+const path = require('path');
+const rimraf = require("rimraf");
 
 // Contains running bots
 var bots = [];
+
+// Paths
+var saveDir = 'saves'
+var localWebDataDir = path.join(__dirname, "webdata")
+// Generate webdata location
+var chromeWebDataDir = process.env.LOCALAPPDATA + "\\Google\\Chrome\\User Data\\Default" ||
+    (process.platform == 'darwin' ? process.env.HOME + 'Library/Application Support/Google/Chrome' : '~/.config/google-chrome')
+
+// Moves webdata and deletes old data
+async function copyWebData(file, dir2) {
+    // Clear previous data dirs
+    //TDOD: Configuration option for clearing previous sessions' data
+    fs.ensureDir(dir2, err => {
+        var f = path.basename(file);
+        var source = fs.createReadStream(file);
+        var dest = fs.createWriteStream(path.resolve(dir2, f));
+
+        source.pipe(dest);
+        source.on('error', function (err) { console.log(err); });
+        return true;
+    });
+};
+
 // Load proxies
 var proxies = fs.readFileSync('proxies.txt').toString().split("\n");
 if (proxies[0] == '') proxies = [];
 
+// Show intro
 logger.intro(config.taskCount, proxies.length);
 
+// Remove saves from the last run
+rimraf.sync(saveDir);
+
+// Launch tasks
 (async () => {
 
     for (let index = 0; index < config.taskCount; index++) {
@@ -20,9 +50,18 @@ logger.intro(config.taskCount, proxies.length);
             bots.push(new Bot(index));
         }
 
-        setTimeout(function() {    
-            bots[index].start();
-        }, config.startUpDelayInterval * index);
+        if (config.webdata.useWebData) {
+            if (config.webdata.path != "") {
+                await copyWebData(config.webdata.path + '\\Web Data', path.resolve('saves', 'chrome_' + index, "Default"));
+            } else {
+                await copyWebData(chromeWebDataDir + '\\Web Data', path.resolve('saves', 'chrome_' + index, "Default"));
+            }
+        }
+        
+        setTimeout(function(i) {
+            bots[i].start();
+        }, config.startUpDelayInterval * index, index);
+
     }
 
 })();
